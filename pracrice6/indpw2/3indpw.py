7a3ed8511e73418dd70b4c26625c52c967d38913
Yerniaz <94180678+Yerniaz@users.noreply.github.com>
2022-09-20 14:15:58 +0600

    Add files via upload

---
+++ pracrice6/indpw2/3indpw.py
@@ -0,0 +1,7 @@
+def to_list(*arg):
+    print ("List of elements: ", arg)
+
+
+a=[1,2,3,4]
+bb=['aaaaaaaaaaaaaaa', 'bbbbbbbbbbbb', 'cccccccccccccccc']
+to_list(1, a, bb)
---
+++ pracrice6/indpw2/indep1.py
@@ -0,0 +1,7 @@
+n = int(input("Length of array: "))
+a = []
+for i in range(n):
+    a.append(int(input()))
+print(a)
+a.reverse()
+print(a)
